8a16be14305fe852fad60ddcc4ceed6b7201edfe
Rhayanny rodrigues <rhayannyrodrigues71 @gmail.com>
2024-03-05 18:43:30 -0300

    feat: att

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
+import Routes from "./routes";
+import "./styles/index.scss";
 
 function App() {
-
   return (
     <>
-     
+      <Routes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
