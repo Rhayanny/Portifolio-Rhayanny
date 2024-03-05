0cbdef4f7dcad4008d5872e37e22e77956584baf
Rhayanny rodrigues <rhayannyrodrigues71 @gmail.com>
2024-03-05 18:36:59 -0300

    feat: projects

---
+++ src/data/Projects/projects.jsx
@@ -0,0 +1,31 @@
+export const projects = [
+  {
+    title: "Kenzie hub",
+    description:
+      "Projeto (FRONTEND) para registro de novas tecnologias, com uma aplicação simples que permite você acompanhar as tecnologias que está desenvolvendo, e os níveis de avanço em cada Tec, permitindo fazer login e cadastro com o consumo de Api. ",
+
+    repository:
+      "https://github.com/Kenzie-Academy-Brasil-Developers/Kenzie-hub-Rhayanny",
+  },
+  {
+    title: "Nu Kenzie",
+    description:
+      " Carteira Digital, uma aplicação (FRONTEND) simples para controle de entrada e saída de capital em uma única carteira.",
+    repository:
+      "https://github.com/Kenzie-Academy-Brasil-Developers/Nu-kenzie-Rhayanny",
+  },
+  {
+    title: "Kenzie Burguer",
+    description:
+      "Lanchonete de hambúrgueres (FRONTEND). Algumas funcionalidades: cadastro e login de usuários, adicionar e remover produtos da sacola, com contador de preço dinâmico.Não é possível adicionar o mesmo lanche mais de uma vez, é disparado um toast alertando que o produto já encontra-se na sacola. ",
+    repository:
+      "https://github.com/Kenzie-Academy-Brasil-Developers/hamburgueriaKenzie-Rhayanny",
+  },
+  {
+    title: "Contacts",
+    description:
+      "Uma aplicação (FULLSTACK) de agenda de contatos, incorporando um sistema CRUD (Create, Read,Update, Delete) que permite aos usuários realizar cadastros e autenticações. Ao autenticar-se, cada usuário obtém acesso à sua própria página privada, onde pode gerenciar suas informações de maneira segura. ",
+    repository:
+      "https://github.com/Kenzie-Academy-Brasil-Developers/DesafioFullstack-Rhayanny",
+  },
+];
