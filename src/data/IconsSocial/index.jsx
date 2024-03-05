03bdf197d6c4f464d1ef917f5e60d5d13c120492
Rhayanny rodrigues <rhayannyrodrigues71 @gmail.com>
2024-03-05 18:36:05 -0300

    feat: icons

---
+++ src/data/IconsSocial/index.jsx
@@ -0,0 +1,16 @@
+import { FaGithub, FaLinkedinIn, FaYoutube, FaWhatsapp } from "react-icons/fa";
+
+export const iconsSocial = [
+  {
+    icon: <FaGithub color="#C1CBD3" size={40} />,
+    href: "https://github.com/Rhayanny",
+  },
+  {
+    icon: <FaLinkedinIn color="#C1CBD3" size={40} />,
+    href: "https://www.linkedin.com/in/rhayanny-rodrigues-76b23728a/",
+  },
+  {
+    icon: <FaWhatsapp color="#C1CBD3" size={40} />,
+    href: "https://api.whatsapp.com/send?phone=5516993273358",
+  },
+];
---
+++ src/data/IconsTech/iconsTech.jsx
@@ -0,0 +1,66 @@
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+  FaReact,
+  FaNodeJs,
+  FaPython,
+  FaSass,
+  FaGitAlt,
+  FaGithub,
+  FaDatabase,
+  FaFigma,
+  FaWordpress,
+} from "react-icons/fa";
+import { SiTypescript, SiAstro, SiNextdotjs } from "react-icons/si";
+
+export const icons = [
+  {
+    icon: <FaHtml5 color="#C1CBD3" />,
+    name: "HTML5",
+  },
+  {
+    icon: <FaCss3Alt color="#C1CBD3" />,
+    name: "CSS3",
+  },
+  {
+    icon: <FaJsSquare color="#C1CBD3" />,
+    name: "Javascript",
+  },
+  {
+    icon: <SiTypescript color="#C1CBD3" />,
+    name: "Typescript",
+  },
+  {
+    icon: <FaReact color="#C1CBD3" />,
+    name: "React",
+  },
+  // {
+  //   icon: <SiNextdotjs color="#C1CBD3" />,
+  //   name: "Next.js",
+  // },
+  {
+    icon: <FaSass color="#C1CBD3" />,
+    name: "Sass",
+  },
+  {
+    icon: <FaGithub color="#C1CBD3" />,
+    name: "GitHub",
+  },
+  {
+    icon: <FaFigma color="#C1CBD3" />,
+    name: "Figma",
+  },
+  {
+    icon: <FaDatabase color="#C1CBD3" />,
+    name: "SQL",
+  },
+  {
+    icon: <FaNodeJs color="#C1CBD3" />,
+    name: "Node.js",
+  },
+  {
+    icon: <FaPython color="#C1CBD3" />,
+    name: "Python",
+  },
+];
